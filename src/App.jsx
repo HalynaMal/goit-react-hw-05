eecca525a6df93f5447ad6810589cd3e60ae0e15
Halyna <dguoit@ukr.net>
2024-04-02 16:35:20 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+//import { useState } from 'react'
+import axios from 'axios';
+
+import './App.css'
+
+function App() {
+  
+  const url = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1';
+
+  const options = {
+   headers: {
+    // Замість api_read_access_token вставте токен  api-key: 3d2eadbfe8af5a118af2934be0f68dff
+  Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZDJlYWRiZmU4YWY1YTExOGFmMjkzNGJlMGY2OGRmZiIsInN1YiI6IjY2MGJlMjY2YTg4NTg3MDE3Y2U2Zjc3ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zJN5mfu1Ld40feDAbvGMXnIhz0cpQyybPbSBDXDUuBw'
+   }
+  };
+  
+  axios.get(url, options)
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+  
+
+
+  return (
+     <div>
+        
+     </div>
+      
+  )
+}
+
+export default App
